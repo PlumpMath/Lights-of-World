d62a24817a27b24863c0f5f270b5582545635dd6
Vinicius A.M. Lobo <vinicius@trustsoftwares.com.br>
2015-10-03 18:03:49 -0300

    Update

--- Lights of World/Sources/Lights of World/sprites.hpp
+++ Lights of World/Sources/Lights of World/sprites.hpp
@@ -15,12 +15,15 @@ struct Sprite {
     Sprite();
     ~Sprite();
     
-    void draw(float size_x, float size_y, float *position_x, float *position_y, const char *file);
-    void animate();
+    void draw(float size_x, float size_y, float *position_x, float *position_y, const char *file, int id=0);
+    void animate(const int sprites_x, const int sprites_y, float velocity, int *current_frame_x, int *current_frame_y);
     
 private:
     
     SDL_Surface *image_;
+    
+    int tex_sprites_x;
+    int tex_sprites_y;
     
     float tex_coord_x;
     float tex_coord_y;
